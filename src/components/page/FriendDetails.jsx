f84b6f64ecc08736bb0d6f24c7cb3af92793c5d8
mhk432 <mdharunk477@gmail.com>
2026-04-16 09:09:02 +0600

    added navbar

--- src/components/page/FriendDetails.jsx
+++ src/components/page/FriendDetails.jsx
@@ -1,17 +1,13 @@
-// import React, { use } from 'react';
-import { useLoaderData, useParams } from 'react-router';
+import { useLoaderData } from 'react-router';
 
-// const allFriendPromis = fetch('/friendsData.json').then(res => res.json())
 
 const FriendDetails = () => {
-    const friendId = useParams();
+    // const friendId = useParams();
     const friend =useLoaderData();
     console.log(friend,'friend');
    
-//     const friends = use(allFriendPromis)
 
-//    console.log(friends,'friends');
-        console.log(friendId,'friemnd');
+        // console.log(friendId,'friemnd');
     return (
         <div>
             <h1>Friend Details</h1>
